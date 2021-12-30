36bf8cab9c7b67b1344fc7fddcab09b2015d5a90
NYBACHOK <nybachokua@gmail.com>
2021-12-30 11:51:25 +0200

    add elastic to compose

--- Scripts/Py.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-zypper addrepo https://download.opensuse.org/repositories/devel:languages:python/openSUSE_Tumbleweed/devel:languages:python.repo
-zypper refresh
-zypper install python-setuptools
